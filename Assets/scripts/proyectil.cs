4f5dbf903d22c94641fd7e06cd81832d86748b86
DaamiAle <daamiale.recoil@gmail.com>
2026-08-26 20:39:52 -0300

    correccion de gitignore y demas

--- Assets/scripts/proyectil.cs
+++ Assets/scripts/proyectil.cs
@@ -1,16 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class proyectil : MonoBehaviour
+
+
+namespace CosmosCritters
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class proyectil : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
