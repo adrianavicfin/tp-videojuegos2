44b0ece10cf482ad6eaa636319b27fa7fd7a2a11
IvanEstrada00 <ivanestrada.design@gmail.com>
2026-08-26 02:51:43 -0300

    Escena prueba de fisicas y movimiento

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{   
+    public float moveForce;
+    public float maxVel = 5f;
+    public float jumpForce;
+    public Rigidbody2D rb;
+
+    private float inputMove;
+    private bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputMove = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputMove = 0f;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+            {
+                inputMove -= 1f;
+            }
+
+            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            {
+                inputMove += 1f;
+            }
+        }
+
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
+        if (inputMove != 0f)
+        {
+            rb.AddForce(Vector2.right * inputMove * moveForce);
+        }
+
+        rb.linearVelocity = new Vector2(
+            Mathf.Clamp(rb.linearVelocity.x, -maxVel, maxVel),
+            rb.linearVelocity.y
+        );
+    }
+
+
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+    
+} 
